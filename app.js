cf489df997226fcf8fc2a25253833998ab5258e3
ubaidullah1204 <ubaidkh451@gmail.com>
2025-06-26 01:27:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const score = [100,  40  ,2, 23]
+score.sort((a, b) => a-b)
+console.log(score);
+
+
+
+
+const object = 
+{
+    name : "ubaid ",
+    clas : "10th",
+    date : "20-5-25"
+
+}
+
+let { name, clas , date}= object
+
+console.log(name, clas, date);
+
+
+
+
